179ffcc1d31a48366160b68d00be4424eaafba1b
handong.xhd <handong.xhd@alibaba-inc.com>
2020-04-13 17:20:12 +0800

    response

---
+++ summer-biz-core/src/main/java/com/summer/biz/core/api/Response.java
@@ -0,0 +1,15 @@
+package com.summer.biz.core.api;
+
+public interface Response<T> {
+
+    boolean isSuccess();
+
+    String errCode();
+
+    String errName();
+
+    String errMsg();
+
+    T getResponseData();
+
+}
