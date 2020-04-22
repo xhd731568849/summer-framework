cd56fbd9fcc300bb75fc4db3a60aeef79e4a5b18
pudge <xhd731568849@163.com>
2020-04-22 14:00:01 +0800

    condition版本

---
+++ algorithm/src/main/java/com/test/kuaishou/Test4.java
@@ -0,0 +1,49 @@
+package com.test.kuaishou;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 3个线程循环打印
+ */
+public class Test4 {
+
+    static int count = 0;
+
+    static Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while (true) {
+                lock.lock();
+                if (count % 3 == 0) {
+                    System.out.println("A");
+                    count++;
+
+                }
+                lock.unlock();
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                lock.lock();
+                if (count % 3 == 1) {
+                    System.out.println("B");
+                    count++;
+                }
+                lock.unlock();
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                lock.lock();
+                if (count % 3 == 2) {
+                    System.out.println("C");
+                    count++;
+                }
+                lock.unlock();
+            }
+        }).start();
+    }
+
+}
---
+++ algorithm/src/main/java/com/test/kuaishou/Test5.java
@@ -0,0 +1,72 @@
+package com.test.kuaishou;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Test5 {
+
+
+    static int count = 0;
+
+    static Lock lock = new ReentrantLock();
+    static Condition c1 = lock.newCondition();
+    static Condition c2 = lock.newCondition();
+    static Condition c3 = lock.newCondition();
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    lock.lock();
+                    while (count % 3 != 0) {
+                        c1.await();
+                    }
+                    System.out.println("A");
+                    count++;
+                    c2.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                try {
+                    lock.lock();
+                    while (count % 3 != 1) {
+                        c2.await();
+                    }
+                    System.out.println("B");
+                    count++;
+                    c3.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                try {
+                    lock.lock();
+                    while (count % 3 != 2) {
+                        c3.await();
+                    }
+                    System.out.println("C");
+                    count++;
+                    c1.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }).start();
+    }
+
+}
