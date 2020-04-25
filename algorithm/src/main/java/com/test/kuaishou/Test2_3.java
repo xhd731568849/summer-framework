b7c2f6ee64060c49c7005ca1e73a5639f13d5179
handong.xhd <handong.xhd@alibaba-inc.com>
2020-04-25 14:56:25 +0800

    再写一遍线程顺序执行

---
+++ algorithm/src/main/java/com/test/kuaishou/Test2_3.java
@@ -0,0 +1,65 @@
+package com.test.kuaishou;
+
+public class Test2_3 {
+
+    static int count = 0;
+    static Object o = new Object();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while (true) {
+                synchronized (o) {
+                    if (count % 3 == 0) {
+                        System.out.println("A");
+                        count++;
+                        o.notifyAll();
+                    } else {
+                        try {
+                            o.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                synchronized (o) {
+                    if (count % 3 == 1) {
+                        System.out.println("B");
+                        count++;
+                        o.notifyAll();
+                    } else {
+                        try {
+                            o.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                synchronized (o) {
+                    if (count % 3 == 2) {
+                        System.out.println("C");
+                        count++;
+                        o.notifyAll();
+                    } else {
+                        try {
+                            o.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+
+    }
+
+}
---
+++ algorithm/src/main/java/com/test/kuaishou/Test2_4.java
@@ -0,0 +1,76 @@
+package com.test.kuaishou;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Test2_4 {
+
+    static int count = 0;
+    static Lock lock = new ReentrantLock();
+    static Condition c1 = lock.newCondition();
+    static Condition c2 = lock.newCondition();
+    static Condition c3 = lock.newCondition();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    lock.lock();
+                    if (count % 3 == 0) {
+                        System.out.println("A");
+                        count++;
+                        c2.signal();
+                    } else {
+                        c1.await();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    lock.lock();
+                    if (count % 3 == 1) {
+                        System.out.println("B");
+                        count++;
+                        c3.signal();
+                    } else {
+                        c2.await();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+
+        }).start();
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    lock.lock();
+                    if (count % 3 == 2) {
+                        System.out.println("C");
+                        count++;
+                        c1.signal();
+                    } else {
+                        c3.await();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+
+        }).start();
+    }
+}
