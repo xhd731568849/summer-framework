93dd3423d12e951864bed79936bc0fd6bcc9935d
handong.xhd <handong.xhd@alibaba-inc.com>
2020-04-26 23:49:32 +0800

    21

--- algorithm/src/main/java/com/test/leetcode/Test21.java
+++ algorithm/src/main/java/com/test/leetcode/Test21.java
@@ -1,4 +1,41 @@
 package com.test.leetcode;
 
+/**
+ * 将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class Test21 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode 这个没看答案竟然写出来了因为这个很类似链表的归并排序(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0);
+        ListNode n = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                n.next = l1;
+                l1 = l1.next;
+            } else {
+                n.next = l2;
+                l2 = l2.next;
+            }
+            n = n.next;
+        }
+        n.next = l1 == null ? l2 : l1;
+        return res.next;
+    }
 }
